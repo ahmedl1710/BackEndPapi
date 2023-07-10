47a01a1e6c4f46bdd1e18aa3c42f7faf58977171
ahmedl1710 <75045795+ahmedl1710@users.noreply.github.com>
2023-07-10 18:22:37 +0100

    hedha lbase, ma tpushi chay fel main

---
+++ src/main/java/com/tn/papibackend/PapiBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.tn.papibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapiBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PapiBackEndApplication.class, args);
+    }
+
+}
